bc92c7a2677b123da9d48d9e2ed04c38e47fafa5
JoshMarBar14 <42632087+JoshMarBar14@users.noreply.github.com>
2020-02-12 14:48:19 +0530

    Initial Commit

---
+++ LinkedListProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedListProject
+//
+//  Created by Josh Barton  on 12/02/20.
+//  Copyright © 2020 Josh Barton . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
